1e99d8835c8c79278a586304f6c79495bcb38992
Luis Boehme <lboehme@hotmail.com>
2022-06-04 17:10:15 -0300

    create basic typescript RN project with geolocation permissions enabled, react-native-paper ui lib and react-navigation lib

---
+++ App.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Provider as PaperProvider } from 'react-native-paper';
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+
+function HomeScreen({ navigation }: HomeProps) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }: DetailsProps) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <PaperProvider>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </PaperProvider>
+    </NavigationContainer>
+  );
+}
+
+export default App;
